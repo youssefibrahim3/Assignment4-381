d79754ea15278df0c6c167364229abe1eabadaaa
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-03-26 10:58:14 -0600

    Added homepage and mainsection

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Footer from "./footer.js"
+import Header from "./header.js"
+import MainSection from "./MainSection.js"
+
+function HomePage()
+{
+    return (
+        <main>
+            <Header/>
+            <MainSection/>
+            <Footer/>
+        </main>
+    );
+}
+export default HomePage;
---
+++ src/components/footer.js
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+function Footer()
+{
+    return (
+    <body>
+        <header>
+            <div>
+                <img src="img/logo.jpeg" alt="Sweet Scoop Ice Cream Logo"/>
+            </div>
+            <div>
+                <h3>Sweet Scoop Ice Cream</h3>
+            </div>
+        </header>
+        <div className="navbar">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element = {<Home/>}/>
+                    <Route path="/" element = {<Home/>}/>
+                    <Route path="/" element = {<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    </body>
+    );
+}
+export default Footer;
