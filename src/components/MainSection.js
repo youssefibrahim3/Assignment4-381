25fc496c3678a11beb2c4e6e66d754dd3011e3ab
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-03-27 23:09:10 -0600

    Edit App.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -6,7 +6,19 @@ function MainSection()
 {
     useEffect();
     return (
-        <h1>tacos</h1>
+        <div className="main-section">
+            <h1>About Sweet Scoop Ice Cream</h1>
+            <p>
+            Sweet Scoop Ice Cream is a family-owned business that has been serving delicious ice cream since 1990. We pride ourselves
+            on using only the freshest ingredients to create our unique flavors. Whether you're in the mood for a classic vanilla or 
+            something more adventurous like our signature "Chocolate Explosion," we have something for everyone. Come visit us and 
+            treat yourself to a sweet scoop today!
+            </p>
+
+            <h1>Featured flavors</h1>
+
+            <h1>Customer Reviews</h1>
+        </div>
     );
 }
 export default MainSection;
