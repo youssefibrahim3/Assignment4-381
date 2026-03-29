c4ba77ab3e49ea96ebdaecb9aa6679e44f01f38c
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-03-28 19:02:57 -0600

    work

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePage from './HomePage.js'
 
 function Header()
 {
@@ -9,14 +8,17 @@ function Header()
 
         <header>
             <div>
-                <img src="img/logo.jpeg" alt="Sweet Scoop Ice Cream Logo"/>
+                <img src="images/logo.jpg" alt="Sweet Scoop Ice Cream Logo"/>
             </div>
             <div>
                 <h3>Sweet Scoop Ice Cream</h3>
             </div>
             <div className="navbar">
+                <a>Home</a>
+                <a>Flavors</a>
+                <a>Login</a>
 
-            </div>
+            </div>  
         </header>
     );
 }
