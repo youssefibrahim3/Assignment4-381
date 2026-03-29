b1157d9b60f32bca9aa244e359651c55c8c66c93
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-03-29 13:46:03 -0600

    Update DisplayStatus.js

--- src/components/DisplayStatus.js
+++ src/components/DisplayStatus.js
@@ -1,17 +1,16 @@
 
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 function DisplayStatus(props)
 {
-    if (props.type == "success")
+    if (props.type === "success")
     {
         return (
             <div style={{color: 'green'}}>
                 <p>{props.message}</p>
             </div>
         );
-    } else if (props.type == "error") {
+    } else if (props.type === "error") {
         return (
             <div style={{color: 'red'}}>
                 <p>{props.message}</p>
