d4e56bde947ff3b96ad729c74741a3beb5b98653
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-03-29 13:39:09 -0600

    complete Login Form

--- src/components/DisplayStatus.js
+++ src/components/DisplayStatus.js
@@ -2,12 +2,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function DisplayStatus()
+function DisplayStatus(props)
 {
-    return (
-        <header>
-
-        </header>
-    );
+    if (props.type == "success")
+    {
+        return (
+            <div style={{color: 'green'}}>
+                <p>{props.message}</p>
+            </div>
+        );
+    } else if (props.type == "error") {
+        return (
+            <div style={{color: 'red'}}>
+                <p>{props.message}</p>
+            </div>
+        );
+    } else {
+        return null;
+    }
 }
 export default DisplayStatus;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,26 +7,82 @@ function LoginForm()
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
     const [messageType, setMessageType] = useState("");
-    useEffect(() => {
+    const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(data => setUsers(data))
+            .catch(() => {
+                setMessage("Error fetching users");
+                setMessageType("error");
+            });
     }, []);
+
+    useEffect(() => {
+        if (messageType === "success")
+        {
+            setTimeout(() => {
+                window.location.href = "/flavors";
+            }, 2000);
+        }
+    }, [messageType]);
+
+    function handleSubmit(button)
+    {
+        button.preventDefault(); //apparently this prevents page refresh
+
+        if (!username)
+        {
+            setMessage("Username cannot be empty.");
+            setMessageType("error");
+            return;
+        }
+
+        if (!password)
+        {
+            setMessage("Password cannot be empty.");
+            setMessageType("error");
+            return;
+        }
+
+        if (password.length < 8)
+        {
+            setMessage("Password must be at least 8 characters.");
+            setMessageType("error");
+            return;
+        }
+
+        const foundUser = users.find(
+            (user) =>
+                user.username === username && user.email === password
+        )
+        if (foundUser)
+        {
+            setMessage("Login successful!");
+            setMessageType("success");
+        } else {
+            setMessage("Invalid username or password");
+            setMessageType("error");
+        }
+    }
 
     return (
         <div>
+            <h1 style={{textAlign: 'center'}}>Login</h1>
             <form id="login_form">
                 <label for="username">Username</label><br/>
-                <input type="text" id="username" name="username" required/><br/><br/>
+                <input type="text" id="username" name="username" 
+                onChange={(e) => setUsername(e.target.value)}/><br/><br/>
 
                 <label for="password">Password (Email)</label><br/>
-                <input type="password" id="password" name="password" required/><br/><br/>
+                <input type="password" id="password" name="password" 
+                onChange={(e) => setPassword(e.target.value)}/><br/><br/>
 
-                <div>
-                    <input type="checkbox" id="forgot_password" name="forgot_password" disabled/>
-                    <label for="forgot_password">Forgot Password?</label><br/><br/>
-                </div>
-
-                <button id="loginbutton">Login</button>
-
+                <button type="submit" onClick={handleSubmit}>Login</button>
+                <br></br>
+                <a>Forgot Password?</a>
+                {message && <DisplayStatus type={messageType} message={message} />}
             </form>
         </div>
     );
