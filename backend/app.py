bac606baac473e157a3bfeb52a6cb698099fa03b
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-10 16:44:29 -0600

    update

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import bcrypt
+
+@app.route("/signup",method=["POST"])
+def createUser():
+
+
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    #username validation
+    if len(username) < 3 or len(username) > 20:
+        return "ERROR: Username must be between 3 and 20 characters."
+    
+    
+    #email validation
+
+    #password validation
---
+++ backend/users.py
@@ -0,0 +1,19 @@
+users = {
+    {
+        "id": 1,
+        "username": "sweet alice",
+        "password_hash": "examplehashedvalue",
+        "cart" : [
+            {
+                "flavorId": 2,
+                "name": "Chocolate Bliss",
+                "price": 5.49,
+                "quantity": 2
+            },
+            ]
+        "orders" : [
+
+        ]
+
+    }
+}
